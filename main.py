7f1c798dae5b81bf6d0fe51b59388bc9d45309de
Jamie Saxon <saxon.jamie@gmail.com>
2020-08-04 18:32:03 +1000

    Core functions working

--- main.py
+++ main.py
@@ -2,45 +2,189 @@ import configparser
 import subprocess
 import time
 import logging
+import os
+import json
+import psutil
 
-conf_file = "config.ini"
-logging.basicConfig(filename='main.log',level=logging.DEBUG,format='%(asctime)s %(message)s')
+acco_conf_file = "config.ini"
+excluded_files = ['acco_default.json', 'configuration.json', 'current']
+logging.basicConfig(filename='main.log', level=logging.DEBUG,
+                    format='%(asctime)s %(levelname)s %(message)s')
 
-def startInstance(instance_id):
-    process = subprocess.Popen(["accserver.exe"], cwd="./instances/" + str(instance_id) + "/",close_fds = True, stdout=subprocess.DEVNULL, stderr=None)
-    return process.pid
 
-## Parse config file
-logging.info("Reading from config file: " + str(conf_file))
-config = configparser.ConfigParser() # Init ConfigParser
-config.read(conf_file) # Parse the config file
-c_instance_limit = int(config.get('general', 'instance_limit')) # Get instance limit from config
-logging.info("Instance limit loaded from config file, Instance limit is: " + str(c_instance_limit))
+def startInstance(serverconfig="default"):
+    # Function to start an accServer instance, this will clear all the directories and start the instance
+    logging.info("Instance start called")
+    instance_id = 0
 
+    # Select an instance to start
+    for instance in instance_status:
+        if (instance_status[instance]["status"] == "available"):
+            instance_id = instance
+
+    # If no available instance found, close any available default instance
+    if instance_id == 0:
+        for instance in instance_status:
+            if (instance_status[instance]["config"] == "default"):
+                instance_id = instance
+                stopInstance(instance_id)
+
+    # If no instances available, error and break
+    if instance_id == 0:
+        logging.error("Instance start called but no available instances")
+        return
+
+    # Clear any existing config files
+    cfg_dir = os.listdir("./instances/" + str(instance_id) + "/cfg/")
+    for curr_file in cfg_dir:
+        if curr_file not in excluded_files:
+            os.remove("./instances/" + str(instance_id) +
+                      "/cfg/" + str(curr_file))
+            # print(os.listdir("./instances/" + str(instance_id) + "/cfg/"))
+    logging.info("Config directory cleared for instance ID: "+str(instance_id))
+
+    # If serverconfig not provided, load default from acco_default.json
+    default = False
+    if (serverconfig == "default"):
+        default = True
+        logging.info("Using default config for instance ID: "+str(instance_id))
+        with open("./instances/" + str(instance_id) + "/cfg/acco_default.json") as json_file:
+            serverconfig = json.load(json_file)
+
+    # Create config files from serverconfig
+    for config_file in serverconfig:
+        if config_file not in excluded_files:
+            with open("./instances/" + str(instance_id) + "/cfg/" + str(config_file), 'w') as fp:
+                json.dump(serverconfig[config_file], fp)
+
+    process = subprocess.Popen(["accserver.exe"], cwd="./instances/" + str(
+        instance_id) + "/", close_fds=True, stdout=subprocess.DEVNULL, stderr=None)
+    # Update instance_status
+    instance_status[instance_id]["pid"] = process.pid
+    instance_status[instance_id]["status"] = "running"
+    if (default == True):
+        instance_status[instance_id]["config"] = "default"
+    else:
+        instance_status[instance_id]["config"] = str(
+            serverconfig["acco.json"]["eventId"])
+        instance_status[instance_id]["timeEnd"] = int(
+            serverconfig["acco.json"]["timeEnd"])
+
+    # Wait up to 30 seconds and confirm instance is started
+    timer = 0
+    while (psutil.pid_exists(process.pid) == False):
+        if (timer >= 30):
+            break
+        timer += 1
+        time.sleep(1)
+
+    # If instance is not running, log an error and update instance_status
+    if (psutil.pid_exists(process.pid) == False):
+        logging.error("Failed to start instance ID "+str(instance_id))
+        instance_status[instance_id]["pid"] = 0
+        instance_status[instance_id]["status"] = "available"
+        instance_status[instance_id]["config"] = 0
+        instance_status[instance_id]["timeEnd"] = 0
+    else:
+        logging.info("Succesfully started instance ID "+str(instance_id) +
+                     " Status: " + str(instance_status[instance_id]))
+
+
+def stopInstance(instance_id):
+    # Function to stop an instance ID and upload results
+    logging.info("Instance stop called for instance ID: " + str(instance_id))
+
+    # TODO Upload results
+
+    pid = instance_status[instance_id]["pid"]
+    p = psutil.Process(pid)
+    psutil.Process.kill(p)
+
+    timer = 0
+    while (psutil.pid_exists(pid) == True):
+        if (timer >= 30):
+            logging.error("Failed to stop instance ID:  "+str(instance_id))
+            return
+        timer += 1
+        time.sleep(1)
+
+    if (psutil.pid_exists(pid) == False):
+        logging.info("Succesfully stopped instance ID:  "+str(instance_id))
+        instance_status[instance_id]["pid"] = 0
+        instance_status[instance_id]["status"] = "available"
+        instance_status[instance_id]["config"] = 0
+        instance_status[instance_id]["timeEnd"] = 0
+
+
+def eventCheck(serverconfig):
+    # Helper to check if an event is running and if not launch it
+    now = time.time()
+
+    # Catch to avoid starting if already running
+    running_check = False
+    for instance in instance_status:
+        if (instance_status[instance]["config"] == serverconfig["acco.json"]["eventId"]):
+            running_check = True
+
+    # Call instance start for serverconfig if:
+    # 1. Start time is in the past
+    # 2. End time is in the future
+    # 3. Status is "scheduled"
+    if int(serverconfig["acco.json"]["timeStart"]) <= now and int(serverconfig["acco.json"]["timeEnd"]) >= now and str(serverconfig["acco.json"]["eventStatus"]) == "scheduled" and running_check == False:
+        startInstance(serverconfig)
+
+
+# Stop any existing accserver.exe instances
+os.system("taskkill /f /im  accServer.exe")
+
+# Parse config file
+logging.info("Reading from config file: " + str(acco_conf_file))
+config = configparser.ConfigParser()  # Init ConfigParser
+config.read(acco_conf_file)  # Parse the config file
+# Get instance limit from config
+c_instance_limit = int(config.get('general', 'instance_limit'))
+logging.info(
+    "Instance limit loaded from config file, Instance limit is: " + str(c_instance_limit))
+
+# Construct instance_status dict
 instance_iter = 1
-instance_status =	{}
+instance_status = {}
 while (instance_iter <= c_instance_limit):
     instance_status[instance_iter] = {
         "status": "available",
-        "pid": 0
+        "pid": 0,
+        "config": 0,
+        "timeEnd": 0
     }
     instance_iter += 1
-    print(instance_status)
+logging.info("Instance status: " + str(instance_status))
 
-startInstance(1)
 
-# running_procs = subprocess.Popen(["accserver.exe"], cwd="./instances/1/",close_fds = True, stdout=subprocess.DEVNULL, stderr=None)
-# running_procs2 = subprocess.Popen(["accserver.exe"], cwd="./instances/2/",close_fds = True, stdout=subprocess.DEVNULL, stderr=None)
+# Main service loop
+while True:
+    logging.info("Instance status: " + str(instance_status))
 
-# while running_procs:
-#     status = running_procs.poll()
-#     if status is not None: # Process finished.
-#         break
-#     else: # No process is done, wait a bit and check again.
-#         time.sleep(1)
-#         continue
+    # Look for events to stop
+    now = time.time()
+    for instance_id in instance_status:
+        if (instance_status[instance_id]["timeEnd"] <= now and instance_status[instance_id]["status"] == "running" and instance_status[instance_id]["config"] != "default"):
+            stopInstance(instance_id)
 
-#     # Here, `running_procs` has finished with return code `retcode`
-#     if status != 0:
-#         """Error handling."""
-#     print(running_procs.stdout)
+    # Look for events to start
+    # TODO Implement DB or API integration
+    for event_file in os.listdir("./testdata/"):
+        if event_file.endswith(".json"):
+            with open("./testdata/" + event_file) as serverconfig_file:
+                serverconfig = json.load(serverconfig_file)
+                # Call the event check and launch function
+                eventCheck(serverconfig)
+
+    # If no scheduled events left, start Default settings on any available instances
+    for instance_num in instance_status:
+        if (instance_status[instance_num]["status"] == "available"):
+            logging.info(
+                "Instance slot available, starting default ")
+            startInstance()
+
+    logging.info("Current status: "+str(instance_status))
+    time.sleep(10)
